0aab58d98ce72529caaa5f3cc47ead7435ff3f35
Lex <lex4vn@gmail.com>
2017-11-22 17:50:19 +0700

    Update news

--- src/NewsScreen/TabSystem.js
+++ src/NewsScreen/TabSystem.js
@@ -1,30 +1,53 @@
 import React from "react";
-import { AppRegistry, Alert } from "react-native";
+import {AppRegistry, Alert} from "react-native";
 
-import {
-  Text,
-  Container,
-  Card,
-  CardItem,
-  Body,
-  Content,
-  Header,
-  Left,
-  Right,
-  Icon,
-  Title,
-  Button,
-  H1
-} from "native-base";
+import {Container, Header, Content, List, ListItem, Text, Thumbnail, Body} from 'native-base';
 
 export default class TabSystem extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Content>
-            <Text>Tin tức giáo dục</Text>
-        </Content>
-      </Container>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            items: []
+        }
+    }
+
+
+    componentDidMount() {
+        return fetch('http://beta.luyentap.vn/frontend/web/api/v1/article/system')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    items: responseJson.items,
+                }, function () {
+                    // do something with new state
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    render() {
+
+        return (
+            <Container>
+                <Content>
+                    <List dataArray={this.state.items}
+                          renderRow={(item) =>
+                              <ListItem>
+                                  <Thumbnail square size={160} source={{uri: (item.absUrl)}}/>
+                                  <Body>
+                                  <Text>{item.title}</Text>
+                                  <Text note>{item.summary}</Text>
+                                  <Text note>{item.published_at}</Text>
+                                  </Body>
+                              </ListItem>
+                          }>
+                    </List>
+                </Content>
+            </Container>
+        );
+    }
 }
