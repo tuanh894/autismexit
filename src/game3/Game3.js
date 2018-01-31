30c102ea53af98cf1117366490568e5cccd75427
AnhTT <trantuanh894@gmail.com>
2018-01-31 22:19:17 +0700

    Game 3

--- src/game3/Game3.js
+++ src/game3/Game3.js
@@ -52,7 +52,7 @@ class Draggable1 extends Component {
 
     render() {
         return (
-            <View style={{ width: "33%",marginTop:'20%', alignItems: "center"}}>
+            <View style={{ width: "100%",marginTop:'20%', alignItems: "center"}}>
                 {this.renderDraggable()}
             </View>
         );
@@ -76,154 +76,9 @@ class Draggable1 extends Component {
     }
 }
 
-class Draggable2 extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            showDraggable: true,
-            dropAreaValues: null,
-            pan: new Animated.ValueXY(),
-            opacity: new Animated.Value(1)
-        };
-    }
 
-    componentWillMount() {
-        this._val = { x:0, y:0 }
-        this.state.pan.addListener((value) => this._val = value);
 
-        this.panResponder = PanResponder.create({
-            onStartShouldSetPanResponder: (e, gesture) => true,
-            onPanResponderGrant: (e, gesture) => {
-                this.state.pan.setOffset({
-                    x: this._val.x,
-                    y:this._val.y
-                })
-                this.state.pan.setValue({ x:0, y:0})
-            },
-            onPanResponderMove: Animated.event([
-                null, { dx: this.state.pan.x, dy: this.state.pan.y }
-            ]),
-            onPanResponderRelease: (e, gesture) => {
-                if (this.isDropArea(gesture)) {
-                    Animated.timing(this.state.opacity, {
-                        toValue: 0,
-                        duration: 1000
-                    }).start(() =>
-                        this.setState({
-                            showDraggable: false
-                        }),
-
-                    );
-                }
-            }
-        });
-    }
-
-    isDropArea(gesture) {
-        return gesture.moveY < 200;
-    }
-
-    render() {
-        return (
-            <View style={{ width: "33%",height:'100%',marginTop:'20%', alignItems: "center"}}>
-                {this.renderDraggable()}
-            </View>
-        );
-    }
-
-    renderDraggable() {
-        const panStyle = {
-            transform: this.state.pan.getTranslateTransform()
-        }
-        if (this.state.showDraggable) {
-            return (
-                <View style={{ position: "absolute" }}>
-                    <Animated.Image
-                        {...this.panResponder.panHandlers}
-                        source={require('../img/3/bi_cam.png')}
-                        style={[panStyle,styles.images_bi, {opacity:this.state.opacity}]}
-                    />
-                </View>
-            );
-        }
-    }
-}
-
-class Draggable3 extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            showDraggable: true,
-            dropAreaValues: null,
-            pan: new Animated.ValueXY(),
-            opacity: new Animated.Value(1)
-        };
-    }
-
-    componentWillMount() {
-        this._val = { x:0, y:0 }
-        this.state.pan.addListener((value) => this._val = value);
-
-        this.panResponder = PanResponder.create({
-            onStartShouldSetPanResponder: (e, gesture) => true,
-            onPanResponderGrant: (e, gesture) => {
-                this.state.pan.setOffset({
-                    x: this._val.x,
-                    y:this._val.y
-                })
-                this.state.pan.setValue({ x:0, y:0})
-            },
-            onPanResponderMove: Animated.event([
-                null, { dx: this.state.pan.x, dy: this.state.pan.y }
-            ]),
-            onPanResponderRelease: (e, gesture) => {
-                if (this.isDropArea(gesture)) {
-                    Animated.timing(this.state.opacity, {
-                        toValue: 0,
-                        duration: 1000
-                    }).start(() =>
-                        this.setState({
-                            showDraggable: false,
-                            lo3: true
-                        })
-                    );
-
-                }
-            }
-        });
-    }
-
-    isDropArea(gesture) {
-        return gesture.moveY < 200;
-    }
-
-    render() {
-        return (
-            <View style={{ width: "33%",marginTop:'20%', alignItems: "center" }}>
-                {this.renderDraggable()}
-            </View>
-        );
-    }
-
-    renderDraggable() {
-        const panStyle = {
-            transform: this.state.pan.getTranslateTransform()
-        }
-        if (this.state.showDraggable) {
-            return (
-                <View style={{ position: "absolute" }}>
-                    <Animated.Image
-                        {...this.panResponder.panHandlers}
-                        source={require('../img/3/bi_do.png')}
-                        style={[panStyle,styles.images_bi, {opacity:this.state.opacity}]}
-                    />
-                </View>
-            );
-        }
-    }
-}
 
 export default class Game3 extends React.Component {
     static navigationOptions = {
@@ -267,8 +122,7 @@ export default class Game3 extends React.Component {
                         </View>
                         <View style={styles.main2}>
                             <Draggable1/>
-                            <Draggable2/>
-                            <Draggable3/>
+
                         </View>
                     </View>
 
